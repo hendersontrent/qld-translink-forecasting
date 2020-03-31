004f850a5ea7c8b8778db8a316b7acc9a25a9a15
hendersontrent <trent.henderson1@outlook.com>
2020-03-31 12:12:43 +1000

    Performed data load and save

---
+++ data-load.R
@@ -0,0 +1,25 @@
+#--------------------------------------------
+# This script sets out to load and clean raw
+# data files and save as a quick-access .Rda
+#--------------------------------------------
+
+#---------------------------------------
+# Author: Trent Henderson, 31 March 2020
+#---------------------------------------
+
+# Read in excel files and bind together
+
+data_files <- list.files("data", full.names = TRUE, pattern = "\\.xlsx", all.files = TRUE)
+
+data.list <- list()
+
+for(i in data_files){
+  df <- read_excel(d)
+  data.list[[i]] <- df
+}
+
+full_data <- rbindlist(data.list, use.names = TRUE)
+
+# Save file for future use
+
+save(full_data, file = "data/full_data.Rda")
